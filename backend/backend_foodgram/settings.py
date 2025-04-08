02bbc7236e9ae1c5496a4b8104b788975ca05cd5
Ilia <utkin-ilia20033@mail.ru>
2025-04-08 23:15:14 +0300

    asdsa

--- backend/backend_foodgram/settings.py
+++ backend/backend_foodgram/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgarm_password'),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
