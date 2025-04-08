771b41d87fa1feb44185fde5c575a0b793ab4733
Ilia <utkin-ilia20033@mail.ru>
2025-04-09 00:23:32 +0300

    asdadsad

--- backend/users/views.py
+++ backend/users/views.py
@@ -19,7 +19,6 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [UpdateOnlyAdmin]
     filter_backends = (filters.SearchFilter,)
     search_fields = ('username',)
     pagination_class = UsersPagination
